52e56c967932934b20c46c9e9f32b167f9bc3ddd
R2 <randyl@techheads.com>
2017-12-07 16:52:56 -0800

    Added header

--- Azure/Stop-AllAzureVMs.ps1
+++ Azure/Stop-AllAzureVMs.ps1
@@ -1,3 +1,8 @@
+#
+#.synopsis "Starts all Azure VM's in the provided ResourceGroup
+#.description I use this script to start all VMs in my Azure Lab resourcegroup.Being able to start and stop all VM's when needed keeps lab costs down.
+#.example   Get-AzureRmVM -ResourceGroupName R2Lab | ForEach {start-azurermvm -name $_.name -ResourceGroupName R2Lab}
+#requires -modules AzureRM 
 function Start-AllAzureVMs () {
     
     [CmdletBinding]
@@ -6,5 +11,3 @@ function Start-AllAzureVMs () {
     $ResourceGroup
 
     Get-AzureRmVM -ResourceGroupName $ResourceGroup | ForEach {stop-azurermvm -name $_.name -ResourceGroupName $ResourceGroup}
-
-# Get-AzureRmVM -ResourceGroupName R2Lab | ForEach {stop-azurermvm -name $_.name -ResourceGroupName R2Lab}
